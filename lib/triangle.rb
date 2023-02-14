a2c2ce24dd32d7dbb4894edf5e83f852d03e2be0
leah wanjiku <leagm22@gmail.com>
2023-02-14 05:04:44 +0300

    complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
-end
+  attr_accessor :a , :b, :c
+  def initialize(a, b, c)
+    @a= a
+    @b =b
+    @c =c
+  end
+  def kind 
+    if [a,b,c].min <=0 || (a+b<=c || a+c<=b || b+c<=a)
+      raise TriangleError
+    elsif a==b && c== b
+       :equilateral
+    elsif a==b || b==c || c==a  
+       :isosceles
+    else
+        :scalene
+    end
+  end
+    
+  class TriangleError < StandardError
+    # triangle error code
+    
+  end
+
+  end
+
